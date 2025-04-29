7b6c9ca9015c960a4d539555f2c1ec031507d2ec
PaulOchoa952 <paalochoa@ittepic.edu.mx>
2025-04-29 12:47:25 -0700

    remove the api project

--- Towatchlist/app/logger.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Request
-from datetime import datetime
-
-
-async def log_request(request: Request):
-    method = request.method
-    endpoint = request.url.path
-    timestamp = datetime.utcnow()
-    print(f"{timestamp} - {method} request to {endpoint}")
